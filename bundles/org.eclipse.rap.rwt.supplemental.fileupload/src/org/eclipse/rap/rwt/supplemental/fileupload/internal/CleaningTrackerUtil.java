f9d11accf6e496718f3b5b0bbe9084e1ce288d69
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-01 22:17:33 +0000

    Extracted handling of apache.fileupload FileCleaningTracker into a dedicated class - bug 362924: Using FileDialog causes classloader leaks https://bugs.eclipse.org/bugs/show_bug.cgi?id=362924

---
+++ bundles/org.eclipse.rap.rwt.supplemental.fileupload/src/org/eclipse/rap/rwt/supplemental/fileupload/internal/CleaningTrackerUtil.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.supplemental.fileupload.internal;
+
+import org.apache.commons.io.FileCleaningTracker;
+import org.eclipse.rwt.RWT;
+import org.eclipse.rwt.service.ISessionStore;
+import org.eclipse.rwt.service.SessionStoreEvent;
+import org.eclipse.rwt.service.SessionStoreListener;
+
+
+class CleaningTrackerUtil {
+
+  static final String TRACKER_ATTR
+    = CleaningTrackerUtil.class.getName().concat( "#cleaningTrackerInstance" );
+  private static final FileUploadCleanupHandler LISTENER = new FileUploadCleanupHandler();
+
+  private CleaningTrackerUtil() {
+    // prevent instantiation
+  }
+
+  public static FileCleaningTracker getCleaningTracker( boolean create ) {
+    FileCleaningTracker tracker;
+    ISessionStore store = RWT.getSessionStore();
+    synchronized( store ) {
+      tracker = ( FileCleaningTracker )store.getAttribute( TRACKER_ATTR );
+      if( tracker == null && create ) {
+        tracker = new FileCleaningTracker();
+        store.setAttribute( TRACKER_ATTR, tracker );
+        store.addSessionStoreListener( LISTENER );
+      }
+    }
+    return tracker;
+  }
+
+  static void stopCleaningTracker( ISessionStore store ) {
+    synchronized( store ) {
+      FileCleaningTracker tracker = ( FileCleaningTracker )store.getAttribute( TRACKER_ATTR );
+      if( tracker != null ) {
+        tracker.exitWhenFinished();
+        store.removeAttribute( TRACKER_ATTR );
+      }
+    }
+  }
+
+  private static class FileUploadCleanupHandler implements SessionStoreListener {
+    public void beforeDestroy( SessionStoreEvent event ) {
+      stopCleaningTracker( event.getSessionStore() );
+    }
+  }
+}
---
+++ tests/org.eclipse.rap.rwt.supplemental.fileupload.test/src/org/eclipse/rap/rwt/supplemental/fileupload/internal/CleaningTrackerUtil_Test.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.supplemental.fileupload.internal;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import junit.framework.TestCase;
+
+import org.apache.commons.io.FileCleaningTracker;
+import org.eclipse.rap.rwt.testfixture.Fixture;
+import org.eclipse.rwt.RWT;
+
+
+public class CleaningTrackerUtil_Test extends TestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    Fixture.setUp();
+    Fixture.fakeNewRequest();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    CleaningTrackerUtil.stopCleaningTracker( RWT.getSessionStore() );
+    Fixture.tearDown();
+  }
+
+  public void testNoCleanerInstanceByDefault() {
+    assertNull( CleaningTrackerUtil.getCleaningTracker( false ) );
+  }
+
+  public void testCleanerIsCreated() {
+    assertNotNull( CleaningTrackerUtil.getCleaningTracker( true ) );
+  }
+
+  public void testCleanerIsBuffered() throws Exception {
+    FileCleaningTracker tracker = CleaningTrackerUtil.getCleaningTracker( true );
+
+    assertSame( tracker, CleaningTrackerUtil.getCleaningTracker( true ) );
+  }
+
+  public void testStopClearsBuffer() throws Exception {
+    CleaningTrackerUtil.getCleaningTracker( true );
+
+    CleaningTrackerUtil.stopCleaningTracker( RWT.getSessionStore() );
+
+    assertNull( CleaningTrackerUtil.getCleaningTracker( false ) );
+  }
+
+  public void testStopCallsExitWhenFinished() throws Exception {
+    FileCleaningTracker tracker = mock( FileCleaningTracker.class );
+    RWT.getSessionStore().setAttribute( CleaningTrackerUtil.TRACKER_ATTR, tracker );
+
+    CleaningTrackerUtil.stopCleaningTracker( RWT.getSessionStore() );
+
+    verify( tracker ).exitWhenFinished();
+  }
+
+  public void testAutoStop() throws Exception {
+    CleaningTrackerUtil.getCleaningTracker( true );
+
+    Fixture.disposeOfServiceContext();
+    Fixture.createServiceContext();
+
+    assertNull( CleaningTrackerUtil.getCleaningTracker( false ) );
+  }
+
+}
