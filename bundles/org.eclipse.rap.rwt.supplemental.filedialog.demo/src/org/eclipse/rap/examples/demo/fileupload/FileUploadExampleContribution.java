671604e7f35d83e3957fd680176cd53aff02b270
Ivan Furnadjiev <ivan@eclipsesource.com>
2012-02-28 15:50:25 +0000

    Fix compile error.

--- bundles/org.eclipse.rap.rwt.supplemental.filedialog.demo/src/org/eclipse/rap/examples/demo/fileupload/FileUploadExampleContribution.java
+++ bundles/org.eclipse.rap.rwt.supplemental.filedialog.demo/src/org/eclipse/rap/examples/demo/fileupload/FileUploadExampleContribution.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2011, 2012 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.rap.examples.demo.fileupload;
 
 import org.eclipse.rap.examples.IExampleContribution;
@@ -12,6 +22,10 @@ final class FileUploadExampleContribution implements IExampleContribution {
 
   public String getTitle() {
     return "File Upload";
+  }
+
+  public String getCategory() {
+    return null;
   }
 
   public IExamplePage createPage() {
