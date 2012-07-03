7810cd5b105e97d393227322bfca84d4dd145dcd
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-07-03 22:48:41 +0200

    Remove obsolete method from interface

--- bundles/org.eclipse.rap.rwt.supplemental.filedialog.demo/src/org/eclipse/rap/examples/demo/fileupload/FileUploadExampleContribution.java
+++ bundles/org.eclipse.rap.rwt.supplemental.filedialog.demo/src/org/eclipse/rap/examples/demo/fileupload/FileUploadExampleContribution.java
@@ -12,7 +12,6 @@ package org.eclipse.rap.examples.demo.fileupload;
 
 import org.eclipse.rap.examples.IExampleContribution;
 import org.eclipse.rap.examples.IExamplePage;
-import org.eclipse.rwt.application.Application;
 
 
 final class FileUploadExampleContribution implements IExampleContribution {
@@ -29,6 +28,4 @@ final class FileUploadExampleContribution implements IExampleContribution {
     return new FileUploadExamplePage();
   }
 
-  public void configure( Application application ) {
-  }
 }
